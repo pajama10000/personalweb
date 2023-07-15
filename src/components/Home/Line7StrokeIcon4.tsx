aff393daf1ab84c0df45e5b5a631df0a5981190c
pajama <99984344+pajama10000@users.noreply.github.com>
2023-07-14 20:24:24 -0400

    e
    e

--- src/components/Home/Line7StrokeIcon4.tsx
+++
@@ -1,9 +0,0 @@
-import { memo, SVGProps } from 'react';
-
-const Line7StrokeIcon4 = (props: SVGProps<SVGSVGElement>) => (
-  <svg preserveAspectRatio='none' viewBox='0 0 239 2' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
-    <path fillRule='evenodd' clipRule='evenodd' d='M239 1.5H0V0.5H239V1.5Z' fill='#66B9E8' />
-  </svg>
-);
-const Memo = memo(Line7StrokeIcon4);
-export { Memo as Line7StrokeIcon4 };
